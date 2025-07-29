be4ef30d330d15eb4b9de14dc0a0329ed4734ec7
Dhayana N. Silva <dhayanadesenvolvimento@gmail.com>
2025-07-29 01:15:08 -0300

    adiciona configuracoes pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(
+    {
+      // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Mão Amiga',
+        short_name: 'Mão Amiga',
+        description: 'Sistema Interativo de Apoio à Assistência de Pessoas em Situação de Rua',
+        theme_color: '#ffffff',
+        // icons: [
+        //   {
+        //     src: 'pwa-192x192.png',
+        //     sizes: '192x192',
+        //     type: 'image/png'
+        //   },
+        //   {
+        //     src: 'pwa-512x512.png',
+        //     sizes: '512x512',
+        //     type: 'image/png'
+        //   }]
+      }
+    }
+  )],
 })
