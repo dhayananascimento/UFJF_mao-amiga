376f6b82dfc1cdb753f98bf89f2365f643320b73
Dhayana N. Silva <dhayanadesenvolvimento@gmail.com>
2025-08-07 01:06:59 -0300

    adiciona pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Mão Amiga',
+        short_name: 'MaoAmiga',
+        description: 'Sistema interativo de apoio à assistência de pessoas em situação de rua.',
+        theme_color: '#ffffff'
+      }
+    })
+  ],
 })
